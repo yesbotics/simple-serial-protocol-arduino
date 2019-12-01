fac24cd4d52575049e05e640c76e5011b27e91b8
Albrecht Nitsche <nitsche@einspunktnull.com>
2019-12-01 14:57:08 +0100

    echo_example: removed empty lines

--- SimpleSerialProtocol/examples/echo_example/echo_example.ino
+++ SimpleSerialProtocol/examples/echo_example/echo_example.ino
@@ -41,31 +41,21 @@ void onReceivedSomething() {
     //
 
     byte byteValue = ssp.readByte();
-
     bool booleanValue = ssp.readBool();
-
     int8_t tinySignedInt = ssp.readInt8();
-
     uint8_t tinyUnsignedInt = ssp.readUnsignedInt8();
-
     int16_t smallSignedInt = ssp.readInt16();
-
     uint16_t smallUnsignedInt = ssp.readUnsignedInt16();
-
     int32_t signedInt = ssp.readInt32();
-
     uint32_t unsignedInt = ssp.readUnsignedInt32();
-
     float floatValue = ssp.readFloat();
-
     char charValue = ssp.readChar();
 
     const int stringBufferSize = 50; // means max. 49 chars length, 1 byte is reserved for end of string byte
     char stringValue[stringBufferSize]; // create buffer char array
-    ssp.readCharArray(stringValue, stringBufferSize); // read vhars from stream, fill buffer
+    ssp.readCharArray(stringValue, stringBufferSize); // read chars from stream, fill buffer
 
     ssp.readEot(); // read and expect the end-of-transmission byte. important, don't forget!
-
 
     //
     // Send answer
@@ -74,23 +64,15 @@ void onReceivedSomething() {
     ssp.writeCommand(COMMAND_ID_SEND); // start command with command id
 
     ssp.writeByte(byteValue);
-
     ssp.writeBool(booleanValue);
-
     ssp.writeInt8(tinySignedInt);
-
     ssp.writeUnsignedInt8(tinyUnsignedInt);
-
     ssp.writeInt16(smallSignedInt);
-
     ssp.writeUnsignedInt16(smallUnsignedInt);
-
     ssp.writeInt32(signedInt);
-
     ssp.writeUnsignedInt32(unsignedInt);
-
     ssp.writeFloat(floatValue);
-
+    ssp.writeChar(charValue);
     ssp.writeCharArray(stringValue);
 
     ssp.writeEot(); // end command with end-of-transmission byte. important, don't forget!

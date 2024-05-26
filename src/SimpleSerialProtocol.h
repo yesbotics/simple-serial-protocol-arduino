634324f04895b640f41057c4d9edd961a1396da6
Albrecht Nitsche <albrechtnitsche@gmail.com>
2024-05-26 16:12:26 +0200

    use common.h

--- src/SimpleSerialProtocol.h
+++ src/SimpleSerialProtocol.h
@@ -7,28 +7,7 @@
 #define SIMPLE_SERIAL_PROTOCOL_H
 
 #include "Core.h"
-
-constexpr unsigned long FLUSH_TIMEOUT = 1000;
-constexpr unsigned long COMMAND_MESSAGE_COMPLETE_TIMEOUT = 1000;
-
-constexpr byte CHAR_EOT = 0x0A; // 10 // End of Transmission - Line Feed Zeichen \n
-
-constexpr byte COMMAND_CALLBACK_RANGE_FROM = 0;
-constexpr byte COMMAND_CALLBACK_RANGE_TO = 255;
-
-constexpr uint8_t ERROR_IS_DEAD = 255;
-constexpr uint8_t ERROR_EOT_WAS_NOT_READ = 254;
-constexpr uint8_t ERROR_IS_NOT_WAITING_FOR_READ_EOT = 253;
-constexpr uint8_t ERROR_IS_NOT_EOT = 252;
-
-constexpr uint8_t ERROR_WAIT_FOR_BYTE_TIMEOUT = 0;
-constexpr uint8_t ERROR_IS_NOT_INITIALIZED = 1;
-constexpr uint8_t ERROR_COMMAND_RANGE_IS_INVALID = 2;
-constexpr uint8_t ERROR_COMMAND_IS_NOT_IN_RESERVED_RANGE = 3;
-constexpr uint8_t ERROR_COMMAND_IS_REGISTERED = 4;
-constexpr uint8_t ERROR_COMMAND_IS_NOT_REGISTERED = 5;
-constexpr uint8_t ERROR_END_OF_STRING_BYTE_NOT_IN_CHAR_ARRAY = 6;
-
+#include "common.h"
 
 class SimpleSerialProtocol final : public Core
 {
